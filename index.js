33a8b22385a19cdadf18f10fe1aa0c232acb097b
Awni-android <awni.hub99@gmail.com>
2024-10-19 17:30:35 +0300

    added comments and Edited on the config.json file

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 import fs from "fs";
-import path, { resolve } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 
+// Fixing "__dirname is not defined in ES module scope" because we made it as a module.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Load configuration from the json file
 const configPath = path.join(__dirname, "config.json");
 const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
 
+// Function to count words in file, skipping words that contain number of syombol
 function countWords(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         if (err.code === "ENOENT") {
+          // Error no Entry/Entity
           reject(`File not found: ${filePath}`);
         } else {
           reject(`Error reading file: ${filePath}`);
         }
         return;
       }
+      // Splitting the text into words
       const words = data.split(/\s+/).filter(Boolean);
 
-      const validWords = words.filter((word) => /^[a-zA-Z]+$/.test(word));
+      // Filtering out words that contain numbers or symbols
+      const validWords = words.filter((word) => /^[a-zA-Z]+$/.test(word)); // filtering
 
       resolve(validWords.length);
     });
   });
 }
 
+// This function to process each file asynchronously.
 async function processFiles() {
   try {
     for (const file of config.files) {
